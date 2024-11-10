ed844e5e844e8df3d3e17276bfe2a52f7b9ff040
presstodash <130226667+presstodash@users.noreply.github.com>
2024-11-10 09:33:08 +0100

    set secure to false
    Website does not deploy correctly on render if secure is set to true.

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET || 'very-secret-randomly-generated-strong-key',
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: process.env.NODE_ENV === 'production',
+    cookie: { secure: false,
         httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000,
         sameSite: 'Lax'
