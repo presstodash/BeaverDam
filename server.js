04459ad3b6dad18e4761e2283d749990bf777c59
presstodash <130226667+presstodash@users.noreply.github.com>
2024-11-10 08:42:21 +0100

    attempted bugfix for login

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.use(session({
     saveUninitialized: false,
     cookie: { secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
-        maxAge: 24 * 60 * 60 * 1000
+        maxAge: 24 * 60 * 60 * 1000,
+        sameSite: 'None'
     }
 }));
 
