02208694a21cc533426a82b5fb57cc0ae2293349
presstodash <130226667+presstodash@users.noreply.github.com>
2024-11-10 09:22:04 +0100

    strengthening cookie for security and testing

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: { secure: false,
-        httpOnly: false,
+        httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000,
         sameSite: 'Lax'
     }
