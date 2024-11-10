a63957b7e743443537f04e528ceda7acf5a806a3
presstodash <130226667+presstodash@users.noreply.github.com>
2024-11-10 09:28:42 +0100

    remove debug print, add prod cookie

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET || 'very-secret-randomly-generated-strong-key',
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false,
+    cookie: { secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000,
         sameSite: 'Lax'
